a9e9748630f0bbfa2a6ec1377a5f153f8cb30261
hrttaras <60965968+hrttaras@users.noreply.github.com>
2020-04-09 16:46:30 +0300

    Update guitar.js

--- Hrytsiv_final-test/guitar.js
+++ Hrytsiv_final-test/guitar.js
@@ -9,21 +9,17 @@ const FREQUENCIES = [329.63, 246.94, 196, 146.83, 110, 82.41];
 function tune(arr) {
     let result = [];
     for (let i = 0; i < 6; i++) {
-        let percentDifference = Math.round(Math.abs(100 - arr[i]*100 / FREQUENCIES[i]).toFixed(2));
+        let percentDifference = Math.abs(100 - arr[i]*100 / FREQUENCIES[i]).toFixed(2);
         if (arr[i] === 0) {
             result.push(ZERO_SIGN);
         } else if (arr[i] > FREQUENCIES[i]) {
-            if (percentDifference < 1) {
-                result.push(OK);
-            } else if (percentDifference >= 1 && percentDifference <= 2) {
+            if (percentDifference > 0 && percentDifference < 3) {
                 result.push(SMALL_HIGH);
             } else {
                 result.push(BIG_HIGH);
             }
         } else if (arr[i] < FREQUENCIES[i]) {
-            if (percentDifference < 1) {
-                result.push(OK);
-            } else if (percentDifference >= 1 && percentDifference <= 2) {
+            if (percentDifference > 0 && percentDifference < 3) {
                 result.push(SMALL_LOW);
             } else {
                 result.push(BIG_LOW);
